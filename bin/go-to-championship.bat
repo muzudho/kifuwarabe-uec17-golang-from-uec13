e3574eb653d14f1331f68fcd6f06927b6c1d7744
muzudho <muzudho1@gmail.com>
2025-11-21 20:23:12 +0900

    go-to-championship.bin

--- bin/go-to-championship.bat
+++ bin/go-to-championship.bat
@@ -8,20 +8,25 @@ echo 全部任せろだぜ（＾～＾）...
 rem プロジェクト・ルートで作業する。
 cd ..
 
-rem プロジェクト・ルートに 📁 `go-to-championship` フォルダーを作成。
-mkdir go-to-championship
+rem プロジェクト・ルートに 📁 `go-to-championship/go-engine-gtp` フォルダーを作成。
+mkdir go-to-championship\go-engine-gtp
 
-rem さっきビルドしてルート・ディレクトリに生成した 📄 `{アプリケーション名}.exe` ファイルを 📁 `go-to-championship` フォルダーへ移動。
-move /Y .\kifuwarabe-uec17-golang-from-uec13.exe .\go-to-championship\
+rem さっきビルドしてルート・ディレクトリに生成した 📄 `{アプリケーション名}.exe` ファイルを 📁 `go-to-championship/go-engine-gtp` フォルダーへ移動。
+move /Y .\kifuwarabe-uec17-golang-from-uec13.exe .\go-to-championship\go-engine-gtp\
 
-rem プロジェクト・ルートにある 📁 `input` フォルダーには設定ファイルが入ってるから、フォルダーを丸ごと 📁 `go-to-championship` フォルダーへコピー。
+rem プロジェクト・ルートにある 📁 `input` フォルダーには設定ファイルが入ってるから、フォルダーを丸ごと 📁 `go-to-championship/go-engine-gtp` フォルダーへコピー。
 rem     /E サブディレクトリ（空のディレクトリーも含める）コピーするオプション。
 rem     /I コピー先がフォルダーの場合に確認しないオプション。
 rem     /Y 上書き確認をしないオプション。
-xcopy /E /I /Y .\input .\go-to-championship\input
+xcopy /E /I /Y .\input .\go-to-championship\go-engine-gtp\input
 
-rem 📁 `go-to-championship` フォルダーの中に 📁 `logs` フォルダー作成。中に 📄 `.gitkeep` ファイルを作成して、空のフォルダーを Git 管理できるようにする。
-mkdir go-to-championship\logs
-echo. > go-to-championship\logs\.gitkeep
+rem 📁 `go-to-championship/go-engine-gtp` フォルダーの中に 📁 `logs` フォルダー作成。中に 📄 `.gitkeep` ファイルを作成して、空のフォルダーを Git 管理できるようにする。
+mkdir go-to-championship\go-engine-gtp\logs
+echo. > go-to-championship\go-engine-gtp\logs\.gitkeep
+
+rem 📁 `go-to-championship/go-engine-gtp` フォルダーの中に 📁 `output` フォルダー作成。中に 📄 `.gitkeep` ファイルを作成して、空のフォルダーを Git 管理できるようにする。
+mkdir go-to-championship\go-engine-gtp\output
+echo. > go-to-championship\go-engine-gtp\output\.gitkeep
 
 echo よし、できたぜ（＾～＾）！
+pause
--- docs/get-started/9-go-to-championship.md
+++ docs/get-started/9-go-to-championship.md
@@ -3,9 +3,7 @@
 
 ## 手順1
 
-📁 `go-to-championship` フォルダーを作成してください。  
-
-
-## 以下のものを１つのフォルダーにまとめて、持って行ってください
-
-* ビルドしてルート・ディレクトリに生成した 📄 `{アプリケーション名}.exe` ファイル
+* 先にビルドしておいてください。
+* 📄 `bin/go-to-championship.bat` ファイルをダブルクリックしください、処理が走ります。  
+* 📁 `go-to-championship` フォルダーが作られます。このフォルダーを大会へ持っていってください。
+* 🌏 [muzudho / kifuwarabe-uec17-cgos-client](https://github.com/muzudho/kifuwarabe-uec17-cgos-client) も出力して、忘れずに持って行ってください。
