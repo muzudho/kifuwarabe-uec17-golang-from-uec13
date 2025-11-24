54b2f33900ca4da86520906bf90ab47feb5d9908
muzudho <muzudho1@gmail.com>
2025-11-24 23:54:32 +0900

    ObserverGameSettingsModel

--- src/model/gamesettingsmodel/observer_game_settings_model.go
+++ src/model/gamesettingsmodel/observer_game_settings_model.go
@@ -1,5 +1,25 @@
 package gamesettingsmodel
 
+import (
+	komi_float "github.com/muzudho/kifuwarabe-uec17-golang-from-uec13/kernel/implementations/part_1_entities/chapter_1_go_conceptual/section_1/komi_float"
+	moves_num "github.com/muzudho/kifuwarabe-uec17-golang-from-uec13/kernel/implementations/part_1_entities/chapter_1_go_conceptual/section_1/moves_num"
+	point "github.com/muzudho/kifuwarabe-uec17-golang-from-uec13/kernel/implementations/part_1_entities/chapter_1_go_conceptual/section_1/point"
+)
+
+const (
+	// Author - 囲碁思考エンジンの作者名だぜ☆（＾～＾）
+	Author = "Satoshi Takahashi"
+)
+
+// Komi - コミ。 6.5 といった数字を入れるだけ。実行速度優先で 64bitに。
+var Komi komi_float.KomiFloat
+
+// MaxMovesNum - 上限手数
+var MaxMovesNum moves_num.MovesNum
+
+// Directions4Array - ４方向（東、西、南、北）の番地。水平方向、垂直方向の順で並べた。Directions4型の順番に対応
+var Directions4Array = [4]point.Point{1, -1, 9, -9}
+
 type ObserverGameSettingsModel struct {
 	// BoardSize - 何路盤
 	boardSize int
